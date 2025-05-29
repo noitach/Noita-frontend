ce4fd42af7417623f66347dfeac752395eb254f2
Damien Chambon <damien.chambon.pro@gmail.com>
2025-05-29 13:10:48 +0200

    Remove unnecessary files and update local config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.otf'],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://noita-backend-982182162415.europe-west1.run.app',
+        changeOrigin: true,
+      },
+      '/images': {
+        target: 'https://noita-backend-982182162415.europe-west1.run.app',
+        changeOrigin: true,
+      },
+    },
+  },
 });
