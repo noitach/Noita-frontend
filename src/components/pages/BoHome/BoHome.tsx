ac87cac0333c95b82b07aa9c67fa2fd46db27fb5
Nicolas Chambon <nicolas.chambon.dev@gmail.com>
2025-06-11 22:35:22 +0200

    revert carousel avec fort volume

--- src/components/pages/BoHome/BoHome.tsx
+++ src/components/pages/BoHome/BoHome.tsx
@@ -29,7 +29,7 @@ const BoHome = () => {
       <BoHeader />
       <main className="BoHome">
         <BoHomeBtn
-          title="Carousel!"
+          title="Carousel"
           icon={<RiCarouselView />}
           link="/admin/carousel"
         />
